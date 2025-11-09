f2e330ace59318b349e6cdd2dcefb7cd5cb6b15a
MOONJUNSEO <answnstj1234@naver.com>
2025-11-09 17:53:03 +0900

    ëëª삭제 : clear

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -3,6 +3,8 @@ package com.back;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
 
 public class App {
     private final Scanner scanner = new Scanner(System.in);;
@@ -19,17 +21,23 @@ public class App {
 
             if (cmd.isBlank()) continue;
 
-            switch (cmd) {
+            Rq rq = new Rq(cmd);
+
+            switch (rq.getActionName()) {
                 case "종료" -> {
                     System.out.println("프로그램을 종료합니다.");
                     break while1;
                 }
                 case "등록" -> actionWrite();
                 case "목록" -> actionList();
+                case "삭제" -> actionDelete(rq);
             }
         }
         scanner.close();
     }
+
+
+
 
     private void actionWrite() {
         System.out.print("명언 : ");
@@ -52,6 +60,22 @@ public class App {
             System.out.printf("%d / %s / %s\n", wiseSaying.getId(),wiseSaying.getAuthor(),wiseSaying.getContent());
         }
     }
+    private void actionDelete(Rq rq) {
+        int id = rq.getParamAsInt("id", -1);
+
+        if (id == -1) {
+            System.out.println("id를 (숫자로) 입력해주세요");
+            return;
+        }
+        boolean deleted = delete(id);
+
+        if (!deleted) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n",id);
+            return;
+        }
+
+        System.out.printf("%d번 명언이 삭제되었습니다.\n",id);
+    }
 
     private List<WiseSaying> findForList() {
         return wiseSayings.reversed();
@@ -63,5 +87,21 @@ public class App {
         wiseSayings.add(wiseSaying);
 
         return wiseSaying;
+    }
+
+    private boolean delete(int id) {
+        int index = findIndexById(id);
+        if (index == -1) return  false;
+
+        wiseSayings.remove(index);
+        return true;
+    }
+
+    private int findIndexById(int id) {
+        return IntStream.range(0, wiseSayings.size())
+                .filter(i -> wiseSayings.get(i).getId() == id)
+                .findFirst()
+                .orElse(-1);
+
     }
 }
