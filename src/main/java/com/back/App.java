6cc60afe6ede662751dcf3dd960c9d73545e778f
MOONJUNSEO <answnstj1234@naver.com>
2025-11-09 17:34:53 +0900

    등록 : clear

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,9 +1,13 @@
 package com.back;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner = new Scanner(System.in);;
+    private int lastId;
+    private List<WiseSaying> wiseSayings =new ArrayList<>();
 
     public void run() {
         System.out.println("== 명언 앱 ==");
@@ -27,12 +31,20 @@ public class App {
     }
 
     private void actionWrite() {
-        System.out.println("명언 : ");
+        System.out.print("명언 : ");
         String content = scanner.nextLine().trim();
-        System.out.println("작가 : ");
+        System.out.print("작가 : ");
         String author = scanner.nextLine().trim();
         WiseSaying wiseSaying = write(content, author);
 
         System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSaying.getId());
+    }
+
+    private WiseSaying write(String content, String author) {
+        int id = ++lastId;
+        WiseSaying wiseSaying= new WiseSaying(id, content, author);
+        wiseSayings.add(wiseSaying);
+
+        return wiseSaying;
     }
 }
